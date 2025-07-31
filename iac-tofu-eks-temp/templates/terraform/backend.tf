5431a7e3d4b7198b3030ea2e62089305c926d957
Paulo César Nunes <paulo_cesar_nunes@hotmail.com>
2025-07-31 00:19:09 +0000

    feat: update

--- iac-tofu-eks-temp/templates/terraform/backend.tf
+++ iac-tofu-eks-temp/templates/terraform/backend.tf
@@ -1,16 +1,2 @@
-terraform {
-  backend "s3" {}
-}
-
 https://github.com/${{ parameters.repoUrl | parseRepoUrl | pick('owner') }}/${{ values.projectName }}
 
-/*
-# EXEC LOCAL
-terraform {
-  backend "s3" {
-    bucket         = "381491977476-backend-iac-opentofu"
-    key            = "pcnuness/containers-blog/eks-adr-kubernetes/cluster-kubernetes/terraform.tfstate"
-    region         = "us-east-1"
-  }
-}
-*/
